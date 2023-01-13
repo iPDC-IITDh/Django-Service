f266530c5eea73b60133cc96d68e41319429f0f7
pavanvpatil <pavanvpatil01@gmail.com>
2023-01-13 14:14:29 +0530

    proper parsig of iPDC config done

--- Parser/config.py
+++ Parser/config.py
@@ -1,103 +1,133 @@
+import json
+
 fp = open("./ipdc9090.bin","rb")
 file = fp.readlines()
-# for i in file:
-#     # print(i)
-
-config = file[-1]
 sourcesCFG = int(file[9].decode().split(" ")[1])
-print("CFG: ",sourcesCFG) 
+# print("CFG: ",sourcesCFG) 
 config_lineno = 9
 
 PMUs_config = []
 
 for j in range(sourcesCFG):
-    dictionary = {}
-    print("CFG: ",j+1)
+    maindictionary = {}
+    # print("CFG: ",j+1)
     config = file[config_lineno+2]
-    print("config size: ",len(config))
+    # print("config size: ",len(config))
     index = 0
     config_lineno+=2
     sync = int.from_bytes(config[index:index+2],byteorder='big',signed=False)
     index+=2
-    print(sync)
-    dictionary["sync"] = sync
+    # print(sync)
+    maindictionary["sync"] = sync
     framesize = int.from_bytes(config[index:index+2],byteorder='big',signed=False)
     index+=2
-    print(framesize)
+    # print(framesize)
+    maindictionary["framesize"] = framesize
     idcode = int.from_bytes(config[index:index+2],byteorder='big',signed=False)
     index+=2
-    print(idcode)
+    # print(idcode)
+    maindictionary["idcode"] = idcode
     soc = int.from_bytes(config[index:index+4],byteorder='big',signed=False)
     index+=4
-    print(soc)
+    # print(soc)
+    maindictionary["soc"] = soc
     fracsec = int.from_bytes(config[index:index+4],byteorder='big',signed=False)
     index+=4
-    print(fracsec)
+    # print(fracsec)
+    maindictionary["fracsec"] = fracsec
     time_base = int.from_bytes(config[index:index+4],byteorder='big',signed=False)
     index+=4
-    print(time_base)
+    # print(time_base)
+    maindictionary["time_base"] = time_base
     num_pmu = int.from_bytes(config[index:index+2],byteorder='big',signed=False)
     index+=2
-    print("num_pmu: ",num_pmu)
-    num_pmu = 1
+    maindictionary["num_pmu"] = num_pmu
+    # print("num_pmu: ",num_pmu)
     for k in range(num_pmu):
-        station = config[index:index+16].decode()
+        key = "pmu"+str(k+1)
+        maindictionary[key] = {}
+        station = config[index:index+16].decode().strip()
         index+=16
-        print(station)
+        # print(station)
+        maindictionary[key]["station"] = station
         idcodepmu = int.from_bytes(config[index:index+2],byteorder='big',signed=False)
         index+=2
-        print(idcodepmu)
+        # print(idcodepmu)
+        maindictionary[key]["idcode"] = idcodepmu
         Format = int.from_bytes(config[index:index+2],byteorder='big',signed=False)
         index+=2
-        print(Format)
+        # print(Format)
+        maindictionary[key]["Format"] = Format
         phnmr = int.from_bytes(config[index:index+2],byteorder='big',signed=False)
         index+=2
-        print(phnmr)
+        # print(phnmr)
+        maindictionary[key]["phnmr"] = phnmr
         anmr = int.from_bytes(config[index:index+2],byteorder='big',signed=False)
         index+=2
-        print(anmr)
+        # print(anmr)
+        maindictionary[key]["anmr"] = anmr
         dgnmr = int.from_bytes(config[index:index+2],byteorder='big',signed=False)
         index+=2
-        print(dgnmr)
+        # print(dgnmr)
+        maindictionary[key]["dgnmr"] = dgnmr
         for i in range(phnmr):
-            channel = config[index:(index+16)].decode()
+            channelkey = "phasor channel"+str(i+1)
+            channel = config[index:(index+16)].decode().strip()
             index += 16
-            print("channel: ",channel)
+            # print("channel: ",channel)
+            maindictionary[key][channelkey] = channel
         for i in range(anmr):
-            channel = config[index:(index+16)].decode()
+            channelkey = "analog channel"+str(i+1)
+            channel = config[index:(index+16)].decode().strip()
             index += 16
-            print("channel: ",channel)
+            # print("channel: ",channel)
+            maindictionary[key][channelkey] = channel
         for i in range(dgnmr):
             for h in range(16):
-                channel = config[index:(index+16)].decode()
+                channelkey = "Digital channel"+str((i*16)+1)
+                channel = config[index:(index+16)].decode().strip()
                 index += 16
-                print("channel: ",channel)
+                # print("channel: ",channel)
+                maindictionary[key][channelkey] = channel
         
         for i in range(phnmr):
-                factor = int.from_bytes(config[index:index+4],byteorder='big',signed=False)
-                index += 4
-                print(factor)
-        for i in range(anmr):
-                factor = int.from_bytes(config[index:index+4],byteorder='big',signed=False)
-                index += 4
-                print(factor)
-        for i in range(dgnmr):
+            factorkey = "phasor factor"+str(i+1)
             factor = int.from_bytes(config[index:index+4],byteorder='big',signed=False)
             index += 4
-            print(factor)
+            # print(factor)
+            maindictionary[key][factorkey] = factor
+
+        for i in range(anmr):
+            factorkey = "analog factor"+str(i+1)
+            factor = int.from_bytes(config[index:index+4],byteorder='big',signed=False)
+            index += 4
+            # print(factor)
+            maindictionary[key][factorkey] = factor
+
+        for i in range(dgnmr):
+            factorkey = "analog factor"+str(i+1)
+            factor = int.from_bytes(config[index:index+4],byteorder='big',signed=False)
+            index += 4
+            # print(factor)
+            maindictionary[key][factorkey] = factor
+
         fnom = int.from_bytes(config[index:index+2],byteorder='big',signed=False)
         index+=2
-        print(fnom)
+        # print(fnom)
+        maindictionary[key]["fnom"] = fnom
         cfg_cnt = int.from_bytes(config[index:index+2],byteorder='big',signed=False)
         index+=2
-        print(cfg_cnt)
+        # print(cfg_cnt)
+        maindictionary[key]["cfg_cnt"] = cfg_cnt
     data_rate = int.from_bytes(config[index:index+2],byteorder='big',signed=False)
     index+=2
-    print("Data_rate: ",data_rate)
+    # print("Data_rate: ",data_rate)
+    maindictionary["data_rate"] = data_rate
     chk = int.from_bytes(config[index:index+2],byteorder='big',signed=False)
     index+=2
-    print(chk)
-    # print("last index: ",index)
-    # print("DATA_RATE", int.from_bytes(config[framesize-4:framesize-2],byteorder='big',signed=False))
-    # print("FNOM", int.from_bytes(config[framesize-8:framesize-6],byteorder='big',signed=False))
-    # # print("somelast bytes: ",config[index:].decode())
+    # print(chk)
+    maindictionary["chk"] = chk
+    PMUs_config.append(maindictionary)
+
+for i in PMUs_config:
+    print(json.dumps(i, indent = 4))
